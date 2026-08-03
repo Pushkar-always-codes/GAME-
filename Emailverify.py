f34b93937a31313da2e0a433aa99a55d37b1c204
Pushkar-always-codes <pushkaranand808@gmail.com>
2026-08-03 20:35:35 +0530

    Done the project

--- Emailverify.py
+++ Emailverify.py
@@ -1,10 +1,16 @@
 print("Welcome !! Here you can check whether your gmail is correct or not .")
 import string
-a=str(string.ascii_lowercase + string.digits) +"@"
-Email=input("Enter the Email = ")
-for i in Email:
-    if i == a:
-        continue
+a=str(string.ascii_lowercase + string.digits) +"@"+"."
+email=input("Enter the Email = ")
+for i in email:
+    if i in a:
+        if email[0].isalpha():
+            if email[-10] is "@" :
+                if email.count("@") == 1:
+                    continue
+
     else:
         print("Wrong")
-        break
+        quit
+
+print("Correct") 
