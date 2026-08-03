f196d874f57d42833ad4068af65dfd9bad628b49
Pushkar-always-codes <pushkaranand808@gmail.com>
2026-08-03 16:16:06 +0530

    continue

--- Emailverify.py
+++ Emailverify.py
@@ -1,2 +1,10 @@
 print("Welcome !! Here you can check whether your gmail is correct or not .")
 import string
+a=str(string.ascii_lowercase + string.digits) +"@"
+Email=input("Enter the Email = ")
+for i in Email:
+    if i == a:
+        continue
+    else:
+        print("Wrong")
+        break
